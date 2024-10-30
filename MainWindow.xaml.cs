7222c27f0aeb8ffebbbce8fb024cc6952a06af15
Kharristov <annawey486@gmail.com>
2024-10-30 22:09:38 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab7_1_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartPulsarAnimation();
+        }
+        private void StartPulsarAnimation()
+        {
+            RadialGradientBrush gradientBrush = pulsarEllipse.Fill as RadialGradientBrush;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0.0,
+                To = 1.0,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            gradientBrush.GradientStops[0].BeginAnimation(GradientStop.OffsetProperty, animation);
+        }
+    }
+}
